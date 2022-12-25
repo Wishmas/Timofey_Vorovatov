e4ce4fa523eadcba3588d292e278623696129492
Wishmas <99253605+Wishmas@users.noreply.github.com>
2022-12-25 21:24:12 +0300

    bot

---
+++ Бот для заучивания слов/config.py
@@ -0,0 +1,12 @@
+
+BOT_TOKEN = '5660869438:AAFDC7XXfzKYuLf5lQxIZ1pqo1k5tEDvy6U'
+
+DB_NAME = "postgres"
+
+DB_USER = "postgres"
+
+DB_PASSWORD = "9lelWish"
+
+DB_HOST = 'localhost'
+
+DB_PORT = '5432'
